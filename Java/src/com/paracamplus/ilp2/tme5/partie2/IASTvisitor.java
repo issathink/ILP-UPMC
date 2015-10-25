3002ab96829f1230916586a80ac6165b83aa0a7f
issathink <root@Issa.local>
2015-10-25 13:47:37 +0100

    tme5 debut

--- Java/src/com/paracamplus/ilp2/tme5/partie2/IASTvisitor.java
+++ Java/src/com/paracamplus/ilp2/tme5/partie2/IASTvisitor.java
@@ -4,7 +4,7 @@ public interface IASTvisitor<Result, Data, Anomaly extends Throwable> extends
 		com.paracamplus.ilp2.tme5.IASTvisitor<Result, Data, Anomaly> {
 
 	Result visit(IASTsuivant iast, Data data) throws Anomaly;
-
+	Result visit(IASTloop iast, Data data) throws Anomaly;
 	Result visit(IASTdernier iast, Data data) throws Anomaly;
 
 }
